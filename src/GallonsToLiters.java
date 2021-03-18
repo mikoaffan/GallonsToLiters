f8f10902c5d0a33588a53b1e37b48fbd931add44
mikoaffan <mikoaf5@gmail.com>
2021-03-18 14:13:45 +0700

    GallonsToLiters

---
+++ src/GallonsToLiters.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GallonsToLiters
+{
+    public static void main( String [] args )
+    {
+// deklarasi data
+        double liters; // jumlah dalam liter
+        double gallons; // jumlah dalam galon U.S.
+// input data
+        Scanner in = new Scanner( System.in );
+        System.out.print( "Berapa banyak Galon U.S ? " );
+        gallons = in.nextDouble( );
+// 3.785 L = 1 U.S. gal.
+        liters = gallons * 3.785;
+// output results
+        System.out.print( liters + " Liter = " );
+        System.out.println( gallons + " Galon U.S." );
+    }
+}
